b80e0fd29d0432ffa7f55d152064824f2507ef4b
Alexsaros <thealexsaro@gmail.com>
2024-10-24 19:29:04 +0200

    Added a Magic 8 Ball command.

--- main.py
+++ main.py
@@ -1688,6 +1688,36 @@ async def hentie(ctx):
     await ctx.send(message)
 
 
+@bot.command(name="8ball", help="Use the magic eight ball to answer your yes-or-no question.")
+async def eight_ball(ctx):
+    log(f"{ctx.author}: {ctx.message.content}")
+
+    answers = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful",
+    ]
+    answer = f"The Magic 8 Ball says: *{random.choice(answers)}*."
+    await ctx.send(answer)
+
+
 @bot.event
 async def on_command_error(ctx, error):
     print("\nEncountered command error:")
