9c1c18a80c7a430c59d670c90d79628987b00cd0
Alexsaros <thealexsaro@gmail.com>
2024-11-23 10:59:47 +0100

    Automatically create a backup of the dataset every 12 hours.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import requests
 import datetime
 import subprocess
 import flask
+import shutil
 from io import BytesIO
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -32,6 +33,8 @@ ITAD_API_KEY = os.getenv("ITAD_API_KEY")
 DATASET_FILE = "dataset.json"
 FREE_TO_KEEP_GAMES_FILE = "free_to_keep_games.json"
 BEDTIME_MP3 = "bedtime.mp3"
+BACKUP_DIRECTORY = "backups"
+MAX_BACKUPS = 20
 
 BEDTIME_LATE_INTERVAL_MINUTES = 10
 EMBED_MAX_FIELDS = 25
@@ -343,6 +346,28 @@ def read_dataset():
             dataset = json.load(file)   # type: dict[str, int, dict]
 
     return dataset
+
+
+def create_backup(file_to_backup=DATASET_FILE):
+    # Ensure the backup directory exists
+    os.makedirs(BACKUP_DIRECTORY, exist_ok=True)
+
+    # Create a new backup with a timestamp
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = os.path.basename(file_to_backup)
+    backup_filepath = os.path.join(BACKUP_DIRECTORY, f"{filename}_{timestamp}.bak")
+    shutil.copy2(file_to_backup, backup_filepath)
+    print(f"Created backup: {backup_filepath}")
+
+    # Get all the backups for the requested file, and sort them from old to new
+    relevant_backups = [os.path.join(BACKUP_DIRECTORY, f) for f in os.listdir(BACKUP_DIRECTORY) if f.startswith(filename) and f.endswith(".bak")]
+    backups = sorted(relevant_backups, key=os.path.getctime)
+
+    # Remove the oldest backups if we have too many
+    while len(backups) > MAX_BACKUPS:
+        oldest_backup = backups.pop(0)
+        os.remove(oldest_backup)
+        print(f"Deleted old backup: {oldest_backup}")
 
 
 def save_dataset(dataset: dict):
@@ -1133,6 +1158,8 @@ async def on_ready():
     scheduler.add_job(update_dataset_steam_prices, "cron", hour="0,6,12,18")
     # Create a job to check for new free-to-keep every 6 hours
     scheduler.add_job(check_free_to_keep_games, "cron", hour="1,7,13,19")
+    # Create a job that makes a backup of the dataset every 12 hours
+    scheduler.add_job(create_backup, "cron", hour="2,14")
 
     log("Finished on_ready()")
 
