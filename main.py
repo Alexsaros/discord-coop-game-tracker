e533c68adacb113e838e992dd3a4388558c7edea
Alexsaros <thealexsaro@gmail.com>
2024-12-28 15:20:16 +0100

    Always treat arguments given to log() as a string.

--- main.py
+++ main.py
@@ -273,6 +273,7 @@ scheduler = AsyncIOScheduler()
 
 
 def log(message):
+    message = str(message)
     print(message)
     with open("log.log", "a", encoding="utf-8") as f:
         f.write(message + "\n")
